929900e31c07501f60256123bd8f0364f7d50139
Chad <chad.lindstrom@gmail.com>
2014-06-18 09:13:24 -0700

    typo ... should be wianbranding.COM not

--- recognition.php
+++ recognition.php
@@ -1,7 +1,7 @@
 <?php
 function isPorcaro($referer)
 {
-        $pattern = '/^http:\/\/(stage\.|www\.|)(wianbranding|porcaro)\.ca/';
+        $pattern = '/^http:\/\/(stage\.|www\.|)(wianbranding\.com|porcaro\.ca)/';
         return 1 == preg_match($pattern, $referer, $matches);
 }
 
@@ -10,7 +10,7 @@ if(! isPorcaro($_SERVER['HTTP_REFERER']) )
 	echo 0;
 }
 
-$to = 'congrats@wianbranding.ca';
+$to = 'congrats@wianbranding.com';
 
 $subject = "You're the best!";
 $name = $_REQUEST['congrats'];
@@ -21,7 +21,7 @@ $from = $_REQUEST['from'];
 if(!empty($name) && !empty($m) && !empty($from))
 {
 	$message = sprintf("Congratulations %s,  \nYou're the best at %s!\n\nYours truly,\n%s", $name, $m, $from);
-	$headers = 'From: Website <website@wianbranding.ca>' . "\r\n" .
+	$headers = 'From: Website <website@wianbranding.com>' . "\r\n" .
 	    'X-Mailer: PHP/' . phpversion();
 
 	mail ( $to , $subject ,$message, $headers );
